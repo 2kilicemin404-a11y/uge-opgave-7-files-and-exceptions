95422a813a9501b65309182fbf8938b08f67977a
eminkilic <kilicemin404@gmail.com>
2026-03-19 13:06:36 +0100

    .

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,20 @@
+import java.util.Scanner;
 public class Main {
+    Scanner scan = new Scanner(System.in);
+
+
+
+   try {
+        public int promptNumber (String msg);
+            System.out.println(msg);
+    }
+       catch(return Integer.parseInt(scan.nextLine()));{
+
+        }
+    }
+
 
 void main() {
-
-
-
-
 
 
 }
