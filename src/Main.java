231edcbfcd61c315a6c643334f62e8bd2ce4cac9
eminkilic <kilicemin404@gmail.com>
2026-03-19 14:24:02 +0100

    opgave 5 i gang

--- src/Main.java
+++ src/Main.java
@@ -33,12 +33,19 @@ public class Main {
 data.add("Espresso, En kraftfuld og koncentreret kaffe, 25.0");
 data.add("Latte, Espresso med dampet mælk, 45.0");
 data.add("Cola, Kold og brusende, 30.0");
+        data.add("Vand, Stille kildevand, gratis");
+
+double samletPris = 0.0;
 for ( String s : data){
     String[] values = s.split(",");
     String name = values[0].trim();
     String beskrivelse = values[1].trim();
-    String pris = values[2].trim();
+    double pris = Double.parseDouble(values[2].trim());
+    samletPris+= pris;
     System.out.println( name + " - " +  beskrivelse +  " " + " " + pris + "Kr" );
+try {
+    data.add("Vand, Stille kildevand, gratis");
+}
 
 
 }
